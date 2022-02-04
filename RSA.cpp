774a477494580fb89475c942c6d05fbd2b670558
1KitCat1 <denischool119@gmail.com>
2022-02-04 11:19:22 +0200

    RSA add GCD extended Euclidean

--- RSA.cpp
+++ RSA.cpp
@@ -5,7 +5,19 @@ struct key{
 	uint64_t m;
 };
 
-
+// a*x + b*y = gcd(a, b)
+int64_t gcdex(int64_t a, int64_t b, int64_t &x, int64_t &y){
+	if(a == 0){  //0*x+b*y=gcd(a,b)
+		x = 0;
+		y = 1;
+		return b;
+	}
+	int64_t xn, yn;
+	int64_t res = gcdex(b % a, a, xn, yn);
+	x = yn - (b / a) * xn;
+	y = xn;
+	return res;	
+}
 
 int main(){
 
